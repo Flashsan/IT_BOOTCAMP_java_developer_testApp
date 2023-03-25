9204811bc6ac98416c3d62d27da93ba1027b69fa
Aliaksandr Hryharovich <algrigr@gmail.com>
2023-03-25 12:02:29 +0300

    Initial commit

---
+++ service/src/test/java/by/itacademy/it_bootcamp/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itacademy.it_bootcamp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
